ec64476b64541b5874788a990b1c64d6d9c51a9d
shubh-verma <shubhvcs@gmail.com>
2025-02-17 01:48:39 +0530

    dynamic  html

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
-  return render(request, 'hello.html')
+  return render(request, 'hello.html', {'name': 'Shubham'})
