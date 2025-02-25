0f024524e345eecaffac12cdce11d5c9878afe83
shubh-verma <shubhvcs@gmail.com>
2025-02-25 13:53:57 +0530

    weather working api apiview and classView

--- weather/urls.py
+++ weather/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from . import views, views_forecast
-from rest_framework.routers import DefaultRouter
-from .views_forecast import WeatherViewSet
+from . import views
+
+# from rest_framework.routers import DefaultRouter
+from .views import WeatherAPIView
 
 urlpatterns = [
     path("", views.weather, name="current_weather"),
-    # path('/forecast', views_forecast.weather, name='forecast_weather'),
+    path("forecast/", WeatherAPIView.as_view(), name="forecast_weather"),
 ]
 
-router = DefaultRouter()
-router.register(r"forecast", WeatherViewSet, basename="forecast")
+# router = DefaultRouter()
+# router.register(r"forecast", WeatherViewSet, basename="forecast")
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,14 @@
 # Create your views here.
 
-from .serializers import WeatherSerializer
+from .serializers import WeatherSerializer, ForecastResponseSerializer
+from rest_framework.decorators import action
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import requests
 import os
 
+FORECAST_WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}"
 CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
 
 
@@ -27,3 +30,23 @@ def weather(request):
 def fetch_weather(city, api_key):
     weather_response = requests.get(CURRENT_WEATHER_URL.format(city, api_key))
     return WeatherSerializer(data=weather_response.json())
+
+
+class WeatherAPIView(APIView):
+    def get(self, request, format=None):
+        api_key = os.environ.get("API_KEY", "")
+        city = request.query_params.get("q")
+
+        if not city:
+            return Response({"error": "City is required"}, status=400)
+
+        daily_forecasts = self.fetch_forecast(city, api_key)
+
+        if daily_forecasts and daily_forecasts.is_valid():
+            return Response(daily_forecasts.data, status=200)
+
+        return Response({"error": "Invalid response from weather API"}, status=500)
+
+    def fetch_forecast(self, city, api_key):
+        response = requests.get(FORECAST_WEATHER_URL.format(city, api_key))
+        return ForecastResponseSerializer(data=response.json())
